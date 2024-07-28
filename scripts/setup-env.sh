6370eab5d40ef166920a3cbcfcc1f5b3f1857e01
Brendan Lees <contact@brendanlees.me>
2024-07-28 11:27:58 +1000

    removed unused omz commented code

--- scripts/setup-env.sh
+++ scripts/setup-env.sh
@@ -6,10 +6,3 @@ apt install zsh git
 
 # starship
 curl -sS https://starship.rs/install.sh | sh
-
-# oh-my-zsh - (do not set default to zsh)
-# sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-
-# rm .zshrc
-#mv .zshrc.pre-oh-my-zsh .zshrc
-# chsh -s $(which zsh)
