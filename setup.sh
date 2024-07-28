adb6807f3aff1d5f4f242207f1c3827b048b580e
Brendan Lees <contact@brendanlees.me>
2024-07-28 11:00:32 +1000

    added apt setup, removed omz as user specific step

--- setup.sh
+++ setup.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 
 # packages
-apt install git neofetch tmux
-
-# chezmoi
-sh -c "$(curl -fsLS get.chezmoi.io)"
+apt update && apt upgrade -y && apt autoremove
+apt install zsh git neofetch
 
 # starship
 curl -sS https://starship.rs/install.sh | sh
 
 # oh-my-zsh - (do not set default to zsh)
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+# sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
