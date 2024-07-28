c97e1e431f10e8d22d63bd7c7f0f938f8fd1acd0
Brendan Lees <contact@brendanlees.me>
2024-07-28 15:17:20 +1000

    add starship auto-yes, zsh default shell

--- run_once_install_deps.sh
+++ run_once_install_deps.sh
@@ -4,7 +4,10 @@
 apt update && apt upgrade -y && apt autoremove
 
 # install packages
-apt install zsh git
+apt install zsh git nerdfetch
 
 # install starship
-curl -sS https://starship.rs/install.sh | sh
+curl -sS https://starship.rs/install.sh | sh -y
+
+# set zsh to default shell
+chsh -s $(which zsh)
