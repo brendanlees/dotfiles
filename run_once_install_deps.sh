b7dc14dab7b8f3cddf1f4d2a01cbf2eb4a6f119b
Brendan Lees <contact@brendanlees.me>
2024-07-28 15:04:28 +1000

    illegal -y removed

--- run_once_install_deps.sh
+++ run_once_install_deps.sh
@@ -4,7 +4,7 @@
 apt update && apt upgrade -y && apt autoremove
 
 # install packages
-apt install zsh git -y
+apt install zsh git
 
 # install starship
-curl -sS https://starship.rs/install.sh | sh 
+curl -sS https://starship.rs/install.sh | sh -y
