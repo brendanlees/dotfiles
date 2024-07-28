84e5520552c3d17909693f66d1ebafd734047de5
Brendan Lees <contact@brendanlees.me>
2024-07-28 15:50:37 +1000

    move oh-my-zsh to deps

--- run_once_install_deps.sh
+++ run_once_install_deps.sh
@@ -9,5 +9,8 @@ apt install zsh git
 # install starship
 sh -c "$(curl -sS https://starship.rs/install.sh)" -- -y
 
+# install oh-my-zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended --keep-zshrc --skip-chsh
+
 # set zsh to default shell
 chsh -s $(which zsh)
