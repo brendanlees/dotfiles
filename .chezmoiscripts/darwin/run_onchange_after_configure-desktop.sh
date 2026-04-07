3ff01cbfb42b0e2f0bb52e15384be4c8c5b675f6
Brendan Lees <brendan@steadydigital.co>
2026-04-07 12:40:21 +1000

    tweak(macos): update default wallpaper choice, bypass if user selected exists

--- .chezmoiscripts/darwin/run_onchange_after_configure-desktop.sh
+++ .chezmoiscripts/darwin/run_onchange_after_configure-desktop.sh
@@ -2,4 +2,9 @@
 
 set -eufo pipefail
 
-osascript -e 'tell application "System Events" to tell every desktop to set picture to "/System/Library/Desktop Pictures/Solid Colors/Space Gray Pro.png" as POSIX file'
+current=$(osascript -e 'tell application "System Events" to tell desktop 1 to get picture' 2>/dev/null || true)
+
+# only set wallpaper if current one is a system default (not a user photo)
+if [[ "$current" == /System/Library/Desktop\ Pictures/* ]]; then
+  osascript -e 'tell application "System Events" to tell every desktop to set picture to "/System/Library/Desktop Pictures/Chroma Blue.madesktop" as POSIX file'
+fi
