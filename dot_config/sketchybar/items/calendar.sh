f594f6fe0f64dd4eaa063ee8763336f2eecb5203
Brendan Lees <brendan@steadydigital.co>
2026-06-28 21:03:42 +1000

    refactor: anchor sketchybar clock on right

--- dot_config/sketchybar/items/calendar.sh
+++ dot_config/sketchybar/items/calendar.sh
@@ -2,6 +2,10 @@
 
 sketchybar --add item calendar right \
   --set calendar \
-    icon.drawing=off \
+    icon="⣿" \
+    icon.font="$FONT:Regular:13.0" \
+    icon.color="$GREY" \
+    icon.padding_left=4 \
+    icon.padding_right=6 \
     update_freq=30 \
     script="$PLUGIN_DIR/calendar.sh"
