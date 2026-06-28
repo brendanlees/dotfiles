57018c17990848f85003afa492dee477c08b3b69
Brendan Lees <brendan@steadydigital.co>
2026-06-28 21:24:04 +1000

    style: tune sketchybar calendar separator spacing

--- dot_config/sketchybar/items/calendar.sh
+++ dot_config/sketchybar/items/calendar.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 sketchybar --add item calendar right \
-  --set calendar \
+    --set calendar \
     icon="⣿" \
     icon.font="$FONT:Regular:13.0" \
     icon.color="$GREY" \
-    icon.padding_left=4 \
-    icon.padding_right=6 \
+    icon.padding_left=6 \
+    icon.padding_right=12 \
     update_freq=30 \
     script="$PLUGIN_DIR/calendar.sh"
