6ce6062d31f7f7f176e699f4c6ce261f9da4106c
Brendan Lees <brendan@steadydigital.co>
2026-06-28 19:41:38 +1000

    feat: show soundsource volume icon in sketchybar

--- dot_config/sketchybar/items/app_soundsource.sh
+++ dot_config/sketchybar/items/app_soundsource.sh
@@ -2,8 +2,10 @@
 
 sketchybar --add item soundsource right \
   --set soundsource \
-    icon=󰕾 \
+    icon="$ICON_VOLUME_UNKNOWN" \
     icon.font="$FONT:Bold:16.0" \
-    icon.color=$ICON_COLOR \
+    icon.color="$GREY" \
     label.drawing=off \
+    update_freq=60 \
+    script="$PLUGIN_DIR/soundsource.sh" \
     click_script="osascript -e 'tell application \"SoundSource\" to activate'"
