ac850c69bf277b0e70b9d585f87194ec67891268
Brendan Lees <brendan@steadydigital.co>
2026-06-29 10:38:11 +1000

    polish(sketchybar): adding globals for radius and padding

--- dot_config/sketchybar/items/spotify.sh
+++ dot_config/sketchybar/items/spotify.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 sketchybar --add item spotify right \
-  --set spotify \
+    --set spotify \
     drawing=off \
     icon=":spotify:" \
     icon.font="sketchybar-app-font:Regular:16.0" \
@@ -11,11 +11,11 @@ sketchybar --add item spotify right \
     background.color="$PILL_BG" \
     background.border_color="$SPOTIFY_COLOR" \
     background.border_width=1 \
-    background.corner_radius=12 \
+    background.corner_radius="$BORDER_RADIUS" \
     background.height=26 \
     background.drawing=on \
-    padding_left=6 \
-    padding_right=6 \
+    padding_left="$PADDINGS" \
+    padding_right="$PADDINGS" \
     update_freq=15 \
     script="$PLUGIN_DIR/spotify.sh" \
     click_script="osascript -e 'tell application \"Spotify\" to activate'"
