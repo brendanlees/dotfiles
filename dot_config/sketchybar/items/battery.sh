0e093139d449c9daf3b3b179ed9a2d43e7d0d03a
Brendan Lees <brendan@steadydigital.co>
2026-06-30 20:21:51 +1000

    refactor: remove border

--- dot_config/sketchybar/items/battery.sh
+++ dot_config/sketchybar/items/battery.sh
@@ -2,13 +2,13 @@
 
 sketchybar --add item battery right \
     --set battery \
-    icon.font="$FONT:Bold:14.0" \
+    icon.font="$FONT:Bold:13.0" \
     icon.color="$GREEN" \
     icon.padding_right=0 \
     label.font="$FONT:Regular:13.0" \
     label.color="$WHITE" \
     background.color="$PILL_BG" \
-    background.border_color="$GREY" \
+    background.border_color="$SURFACE" \
     background.border_width=1 \
     background.corner_radius=8 \
     background.height="$PILL_HEIGHT" \
